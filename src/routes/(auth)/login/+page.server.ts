0c5fd56099cb75c3a6b3344d4e824ef0de36eeea
thaile27 <122494052+thaile27@users.noreply.github.com>
2024-02-11 21:22:03 -0700

    Initial commit

---
+++ src/routes/(auth)/login/+page.server.ts
@@ -0,0 +1,36 @@
+import { fail, redirect } from '@sveltejs/kit'
+import { superValidate, setError } from 'sveltekit-superforms/server'
+
+import { auth } from '$lib/server/auth'
+import { authSchema } from '$lib/zod/schema'
+
+// if the user exists, redirect authenticated users to the profile page
+export async function load({ locals }) {
+	const { session } = await locals.auth.validateUser()
+	if (session) throw redirect(302, '/')
+
+	const form = await superValidate(authSchema)
+	return { form }
+}
+
+export const actions = {
+	async default({ request, locals }) {
+		const form = await superValidate(request, authSchema)
+
+		if (!form.valid) {
+			return fail(400, { form })
+		}
+
+		try {
+			const key = await auth.useKey(
+				'username',
+				form.data.username,
+				form.data.password
+			)
+			const session = await auth.createSession(key.userId)
+			locals.auth.setSession(session)
+		} catch (error) {
+			return setError(form, 'username', 'Invalid credentials')
+		}
+	},
+}
